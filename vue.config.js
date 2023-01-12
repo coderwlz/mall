06bf5d48093a0648251e5f4bbea96901a7c1bc5d
wanglongzhen <1962955586@qq.com>
2023-01-12 14:46:44 +0800

    构建

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,20 @@
 module.exports = {
-    configureWebpack:{
-        resolve:{
-            alias:{
-                'assets':'@/assets',
-                'common':'@/common',
-                'components':'@/components',
-                'network':'@/network',
-                'views':'@/views'
+    assetsDir: 'static',
+    parallel: false,
+    publicPath: './',
+    configureWebpack: {
+        resolve: {
+            alias: {
+                'assets': '@/assets',
+                'common': '@/common',
+                'components': '@/components',
+                'network': '@/network',
+                'views': '@/views'
             }
         },
-        devServer:{
-            port:5000,//端口号
-            open:true, //是否自启动
+        devServer: {
+            port: 5000,//端口号
+            open: true, //是否自启动
         },
     }
 }
